da8092dacde3780736a44f90f196f33ad43bca5e
012e <huyphmnhat@gmail.com>
2022-11-27 13:58:15 +0700

    k12-bot: botdriver cleanup

--- src/k12-bot/botdriver.py
+++ src/k12-bot/botdriver.py
@@ -9,11 +9,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import logging as log
-import sys
 import time
-import yaml
-import gzip
-from ansparser import parse
 import os
 
 import logging as log
@@ -69,22 +65,33 @@ class BotDriver:
 
     def click_contains(self, type: str, text: str):
         log.info(f"clicking on {text} (type {type})")
-        time.sleep(0.1)
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located(
-                (By.XPATH, f"//{type}[contains(text(), '{text}')]")
-            )
-        ).click()
+        self.click_after_clickable_xpath(f"//{type}[contains(text(), '{text}')]")
 
-    def check_exists_by_xpath(self, xpath) -> tuple[bool, None | WebElement]:
+    def check_exists_by_xpath(self, xpath) -> None | WebElement:
         time.sleep(1)
         try:
             element = self.driver.find_element(By.XPATH, xpath)
         except NoSuchElementException:
-            return (False, None)
-        return (True, element)
+            return None
+        return element
 
-    def click_after_appearance(self, xpath: str):
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, xpath))
-        ).click()
+    def wait_until_clickable(self, element: WebElement):
+        for _ in range(0, 10):
+            try:
+                element.click()
+                return
+            except:
+                pass
+            time.sleep(1)
+
+    def wait_until_clickable_xpath(self, xpath: str) -> WebElement:
+        return WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, xpath))
+        )
+
+    def click_after_clickable(self, element: WebElement):
+        self.wait_until_clickable(element)
+        element.click()
+
+    def click_after_clickable_xpath(self, xpath: str):
+        self.wait_until_clickable_xpath(xpath).click()
